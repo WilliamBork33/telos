87e653b770356734a24e0ce6d7c6c8be72015e84
WilliamBork33 <williambork33@gmail.com>
2018-11-09 12:14:07 -0600

    Routing complete

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "Telos"
+  end
+  
   test "should get index" do
-    get welcome_index_url
+    get welcome_path
     assert_response :success
+    assert_select "title", "Welcome | #{@base_title}"
   end
 
 end
